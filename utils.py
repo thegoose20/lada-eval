76178bfca7de88f391436893ee2ad67a8d463cc0
thegoose20 <hello@lucyhavens.com>
2025-05-07 18:59:20 -0700

    Writing Schema.org and CIDOC-CRM json data

--- utils.py
+++ utils.py
@@ -1,13 +1,13 @@
 import os, re
 
 """
-Write the Dublin Core XML records to a directory.
+Write XML records to a directory.
 """
-def write_dc_xml(records_ids_list, dc_records_list, dc_directory):
+def write_xml(records_ids_list, records_list, dir, file_prefix):
     i, maxI = 0, len(records_ids_list)
     while i < maxI:
         id = str(int(records_ids_list[i]))
-        record = dc_records_list[i]
+        record = records_list[i]
         
         """
         Remove extraneous text so only data between tags (< >) remains
@@ -20,7 +20,6 @@ def write_dc_xml(records_ids_list, dc_records_list, dc_directory):
         to an XML file
         """
         if len(xml_data) > 0:
-            file_prefix = "dc_record_"
             file_suffix = ".xml"
             if len(id) == 1:
                 file_id = "00" + id
@@ -29,10 +28,49 @@ def write_dc_xml(records_ids_list, dc_records_list, dc_directory):
             else:
                 file_id = id
             filename = file_prefix + file_id + file_suffix
-            filepath = dc_directory+filename
+            filepath = dir+filename
 
             with open(filepath, "w") as file:
                 for line in xml_data:
+                    file.write(line)
+                    file.write("\n")
+            file.close()
+            print("Wrote", filename+"!")
+
+        i += 1
+
+"""
+Write JSON records to a directory.
+"""
+def write_json(records_ids_list, records_list, dir, file_prefix):
+    i, maxI = 0, len(records_ids_list)
+    while i < maxI:
+        id = str(int(records_ids_list[i]))
+        record = records_list[i]
+        
+        """
+        Remove extraneous text so only data between curly braces ({ }) remains
+        """
+        json_data = re.findall(r"\{[\W\w]*\}", record)
+
+        """
+        If json_data isn't an empty list, define the file name, 
+        padding the ID with leading zeros, and write the data
+        to a JSON file
+        """
+        if len(json_data) > 0:
+            file_suffix = ".json"
+            if len(id) == 1:
+                file_id = "00" + id
+            elif len(id) == 2:
+                file_id = "0" + id
+            else:
+                file_id = id
+            filename = file_prefix + file_id + file_suffix
+            filepath = dir+filename
+
+            with open(filepath, "w") as file:
+                for line in json_data:
                     file.write(line)
                     file.write("\n")
             file.close()
